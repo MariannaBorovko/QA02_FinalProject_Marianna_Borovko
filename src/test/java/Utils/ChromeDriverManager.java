3f8c936c73e324708d10daaded5d578dc2fc8852
Marianna Borovko <mariborovko@gmail.com>
2021-02-06 16:05:40 +0300

    ChromeOptions added

--- src/test/java/Utils/ChromeDriverManager.java
+++ src/test/java/Utils/ChromeDriverManager.java
@@ -2,16 +2,26 @@ package Utils;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.DesiredCapabilities;
 
 public class ChromeDriverManager extends DriverManager {
 
     protected void createDriverWindows() {
         WebDriverManager.chromedriver().win().setup();
-        driver = new ChromeDriver();
+        DesiredCapabilities desiredCapabilities = DesiredCapabilities.chrome();
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("test-type");
+        desiredCapabilities.setCapability(ChromeOptions.CAPABILITY, options);
+        driver = new ChromeDriver(options);
     }
 
     protected void createDriverLinux() {
         WebDriverManager.chromedriver().linux().setup();
-        driver = new ChromeDriver();
+        DesiredCapabilities desiredCapabilities = DesiredCapabilities.chrome();
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("test-type");
+        desiredCapabilities.setCapability(ChromeOptions.CAPABILITY, options);
+        driver = new ChromeDriver(options);
     }
 }

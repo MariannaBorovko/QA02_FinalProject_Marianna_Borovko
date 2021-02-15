eb77adc65c7e3687b4baf4620b8ae3c6763497b5
Marianna Borovko <mariborovko@gmail.com>
2021-02-15 22:21:38 +0300

    Удалены logs

--- src/test/java/tests/BaseTest.java
+++ src/test/java/tests/BaseTest.java
@@ -21,35 +21,22 @@ public class BaseTest {
 
     @BeforeMethod
     public void setUp() {
-        log.error("1) BasePage driver");
         basePage = new BasePage(driver);
-        log.error("2) DriverManagerFactory get");
         driverManager = basePage.getDriverManagerFactory();
-        log.error("3) Start if");
         if (driverManager.getOS().contains("Win")) {
-            log.error("4) GetWindowsChromeDriver");
             driver = driverManager.getDriverWindows();
         } else {
-            log.error("5) GetLinuxChromeDriver");
             driver = driverManager.getDriverLinux();
         }
-        log.error("6) MainPage");
         mainPage = new MainPage(driver);
-        log.error("7) LoginPage");
         loginPage = new LoginPage(driver);
-        log.error("8) NewsPage");
         newsPage = new NewsPage(driver);
-        log.error("9) MainPage");
         userProfilePage = new UserProfilePage(driver);
-        log.error("10) JobsPage");
         jobsPage = new JobsPage(driver);
-        log.error(" ");
-
     }
 
     @AfterMethod(alwaysRun = true)
     public void closeBrowser() {
-        log.debug("11) QuitDriver");
         driverManager.quitDriver();
     }
 

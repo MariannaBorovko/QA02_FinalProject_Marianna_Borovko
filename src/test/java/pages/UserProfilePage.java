a2aefd002c8730fadb97f523945988503b7dd460
Marianna Borovko <mariborovko@gmail.com>
2021-02-24 22:51:49 +0300

    test 25

--- src/test/java/pages/UserProfilePage.java
+++ src/test/java/pages/UserProfilePage.java
@@ -1,6 +1,7 @@
 package pages;
 
 import lombok.extern.log4j.Log4j2;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
@@ -85,5 +86,6 @@ public class UserProfilePage extends MainPage {
         if (spinner.isDisplayed()) {
             getWebDriverWait().until(ExpectedConditions.invisibilityOf(spinner));
         }
+        getWebDriverWait().until(ExpectedConditions.visibilityOfAllElementsLocatedBy(By.xpath("//div[@class='container']")));
     }
 }
